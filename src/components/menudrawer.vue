93e9afbd9e61536c57da2c4e204a62caa99af71c
ElkinDev <elkinmendoza00@gmail.com>
2020-05-05 17:16:24 +1000

    new adjusments@

--- src/components/menudrawer.vue
+++ src/components/menudrawer.vue
@@ -8,9 +8,47 @@
       width="80%"
     >
       
+  <v-container>
+    <v-row>
+    <v-row class="pt-4 pl-12 pr-12 text-left" :alignment="space-around">
+    <v-col class="col">
+      LOGO
+    </v-col>
+    <v-col class="col text-right">
+      X
+    </v-col>
+    </v-row>
+    
+    <v-col class="col-12 pa-11">
+      <v-list>
+        <v-list-group
+          v-for="item in items"
+          :key="item.title"
+          v-model="item.active"
+          no-action
+        >
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.title"></v-list-item-title>
+            </v-list-item-content>
+          </template>
+  
+          <v-list-item
+            v-for="subItem in item.items"
+            :key="subItem.title"
+            
+          >
+            <v-list-item-content>
+              <v-list-item-title v-text="subItem.title"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
+      </v-list>
+    </v-col>
 
-      <v-divider></v-divider>
+    </v-row>
 
+  </v-container>
      
     </v-navigation-drawer>
 
@@ -28,10 +66,60 @@ export default {
   },
   data: () => ({
     drawer:null,
-        items: [
-          { title: 'Home', icon: 'dashboard' },
-          { title: 'About', icon: 'question_answer' },
-        ],
+         items: [
+        {
+          action: 'local_activity',
+          title: 'Attractions',
+          items: [
+            { title: 'List Item' },
+          ],
+        },
+        {
+          action: 'restaurant',
+          title: 'Dining',
+          active: true,
+          items: [
+            { title: 'Breakfast & brunch' },
+            { title: 'New American' },
+            { title: 'Sushi' },
+          ],
+        },
+        {
+          action: 'school',
+          title: 'Education',
+          items: [
+            { title: 'List Item' },
+          ],
+        },
+        {
+          action: 'directions_run',
+          title: 'Family',
+          items: [
+            { title: 'List Item' },
+          ],
+        },
+        {
+          action: 'healing',
+          title: 'Health',
+          items: [
+            { title: 'List Item' },
+          ],
+        },
+        {
+          action: 'content_cut',
+          title: 'Office',
+          items: [
+            { title: 'List Item' },
+          ],
+        },
+        {
+          action: 'local_offer',
+          title: 'Promotions',
+          items: [
+            { title: 'List Item' },
+          ],
+        },
+      ],
     //
   }),
   methods:{
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -6,12 +6,7 @@ Vue.use(Vuetify);
 export default new Vuetify({
     theme: {
         themes: {
-          light: {
-            primary: '#f5e6e8ff',
-            secondary: '#b0bec5',
-            accent: '#8c9eff',
-            error: '#b71c1c',
-          },
+          light:true
         },
       },
 });
